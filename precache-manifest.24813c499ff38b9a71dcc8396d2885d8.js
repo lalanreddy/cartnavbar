fa92946380b47aaa814df84987e790af35cfa495
lalanreddy <lalanreddy1605@gmail.com>
2020-10-19 14:22:44 +0530

    Updates

---
+++ precache-manifest.24813c499ff38b9a71dcc8396d2885d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a47878ea31664316b9ddde4b3bbe35f",
+    "url": "/cartnavbar/index.html"
+  },
+  {
+    "revision": "396a538ef768ecd0f641",
+    "url": "/cartnavbar/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "6cfdba179c9868d492fa",
+    "url": "/cartnavbar/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "396a538ef768ecd0f641",
+    "url": "/cartnavbar/static/js/2.e15f8438.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/cartnavbar/static/js/2.e15f8438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cfdba179c9868d492fa",
+    "url": "/cartnavbar/static/js/main.a4fde186.chunk.js"
+  },
+  {
+    "revision": "81c560cada39564872f4",
+    "url": "/cartnavbar/static/js/runtime-main.58ed8f54.js"
+  }
+]);
